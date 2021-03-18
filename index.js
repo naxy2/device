1dd2f83479b0ad494d627449b26558f59c2afaef
naxy2 <mr.dieonni@gmail.com>
2021-03-18 11:59:41 +0100

    reso funzionante, diviso in funzioni e aggiunto il loop

--- index.js
+++ index.js
@@ -1,32 +1,57 @@
 const request = require('request');
 require('dotenv').config()
+deviceToken = process.env.token;
+OAuth = process.env.OAuth;
 
-var post_data = {
-    device: process.env.token,
-    date: new Date().toGMTString(),
-    signals: []
-};
+function generaPostData(){
+    data = new Date().toGMTString()
+    var post_data = {
+        device: deviceToken,
+        date: data,
+        signals: []
+    };
+    return post_data;
+}
 
-var signal_info = {
-    name: "numeroCasuale",
-    value: Math.random(0,100)
-};
-post_data.signals.push(signal_info);
+function inserisciValore(post_data){
+    valore = Math.floor(Math.random()*100);
+    var signal_info = {
+        name: "numeroCasuale",
+        value: valore
+    };
+    post_data.signals.push(signal_info);
+    return valore;
+}
 
-var options = {
-    uri: 'https://api.databoom.com/v1/signals/push',
-    method: 'POST',
-    headers: {
-        'Authorization': 'Bearer ' + process.env.chiaveAPI
-    },
-    json: post_data
-};
+function generaOpzioni(post_data){
+    var options = {
+        uri: 'https://api.databoom.com/v1/signals/push',
+        method: 'POST',
+        headers: {
+            'Authorization': 'Bearer ' + OAuth
+        },
+        json: post_data
+    };
+    return options;
+}
 
-request(options, (error, response, body) => {
-    if (error){
-        console.log(`ERRORE: ${error}`);
-        return
+function invia(options){
+    request(options, (error, response, body) => {
+        if (error){
+            console.log(`ERRORE: ${error}`);
+            return
+        }
+        //console.log("ok");
+    });
+}
+
+loop = setInterval(()=>{
+    post_data = generaPostData();
+    for (i=0;i<1;i++){
+        valore = inserisciValore(post_data);
+        console.log(valore);
     }
-    console.log("RISPOSTA: ");
-    console.log(body);
-});
+    opzioni = generaOpzioni(post_data);
+    invia(opzioni);
+}, 10000)
+
